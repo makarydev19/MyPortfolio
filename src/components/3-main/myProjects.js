e5c77f87e7524f6116563916a7d8da61d0160ec6
makary-yasser <makaryasser23@gmail.com>
2024-11-10 02:47:58 +0200

    remove modern website

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,55 +1,48 @@
 export const myProjects = [
   {
-    projectTitle: "Modern Construction Comp..",
-    category: ["nextJS", "ts"],
-    imgPath: "./images/MCC (2).png",
-    link: "https://github.com/makarydev19/MCC.git",
+    projectTitle: 'Modern Construction Comp..',
+    category: ['nextJS', 'ts'],
+    imgPath: './images/MCC (2).png',
+    link: 'https://github.com/makarydev19/MCC.git',
   },
 
   {
-    projectTitle: "Pig Game",
-    category: ["js", "css"],
-    imgPath: "./images/Pig-Game.png",
-    link: "https://github.com/makarydev19/Pig-Game.git",
+    projectTitle: 'Pig Game',
+    category: ['js', 'css'],
+    imgPath: './images/Pig-Game.png',
+    link: 'https://github.com/makarydev19/Pig-Game.git',
   },
 
   {
-    projectTitle: "Guess My Number",
-    category: ["js", "css"],
-    imgPath: "./images/Guess-my-number.png",
-    link: "https://github.com/makarydev19/Guess-my-number.git",
+    projectTitle: 'Guess My Number',
+    category: ['js', 'css'],
+    imgPath: './images/Guess-my-number.png',
+    link: 'https://github.com/makarydev19/Guess-my-number.git',
   },
 
   {
-    projectTitle: "Modern Portfolio",
-    category: ["nextJS", "ts"],
-    imgPath: "./images/Portfoio.png",
-    link: "https://github.com/makarydev19/Portfolio.git",
+    projectTitle: 'Weather App',
+    category: ['js', 'css'],
+    imgPath: './images/Weather-App.png',
+    link: 'https://github.com/makarydev19/Weather-App.git',
   },
 
   {
-    projectTitle: "Weather App",
-    category: ["js", "css"],
-    imgPath: "./images/Weather-App.png",
-    link: "https://github.com/makarydev19/Weather-App.git",
-  },
-
-  {
-    projectTitle: "Space Tourism Website",
-    category: ["js", "css"],
-    imgPath: "./images/Space (2).png",
-    link: "https://github.com/makarydev19/Space-Tourism-Website.git",
+    projectTitle: 'Space Tourism Website',
+    category: ['js', 'css'],
+    imgPath: './images/Space (2).png',
+    link: 'https://github.com/makarydev19/Space-Tourism-Website.git',
   },
   {
-    projectTitle: "To Do List",
-    category: ["js", "css"],
-    imgPath: "./images/To-Do-List.png",
-    link: "https://github.com/makarydev19/To-Do-List.git",
+    projectTitle: 'To Do List',
+    category: ['js', 'css'],
+    imgPath: './images/To-Do-List.png',
+    link: 'https://github.com/makarydev19/To-Do-List.git',
   },
   {
-    projectTitle: "Farsha Mountain Lounge",
-    category: ["react", "js"],
-    imgPath: "./images/FarshaMountainLounge.png",
-    link: "https://github.com/makarydev19/Farsha-Mountain-Lounge.git",
+    projectTitle: 'Farsha Mountain Lounge',
+    category: ['react', 'js'],
+    imgPath: './images/FarshaMountainLounge.png',
+    link: 'https://github.com/makarydev19/Farsha-Mountain-Lounge.git',
   },
 ];
